075191cbf0fd8e65fadba0d6277a6aed60c856d5
Carlos Carvalho <carlos@octohive.cloud>
2025-10-17 00:09:42 -0300

    feat:Configures start odoo script

--- modules/compute_engine/scripts/startup-odoo.sh
+++ modules/compute_engine/scripts/startup-odoo.sh
@@ -37,36 +37,7 @@ ensure_dir() {
 export DEBIAN_FRONTEND=noninteractive
 trap 'handle_error $LINENO' ERR
 
-
-log "##################"
-log "Otimizando o sistema operacional..."
-add_unique_line "vm.dirty_ratio=6" /etc/sysctl.conf
-add_unique_line "vm.dirty_background_ratio=3" /etc/sysctl.conf
-add_unique_line "vm.vfs_cache_pressure=50" /etc/sysctl.conf
-add_unique_line "vm.swappiness=10" /etc/sysctl.conf
-add_unique_line "vm.max_map_count=262144" /etc/sysctl.conf
-add_unique_line "fs.file-max=2097152" /etc/sysctl.conf
-sysctl -p
-log "##################"
-log "Iniciando configuração do Odoo..."
-log "##################"
-
-log "Atualiza sistema e instala gcsfuse"
-if ! command -v gcsfuse &>/dev/null; then
-    apt-get update && apt upgrade -y
-    apt-get install -y curl gnupg lsb-release fuse3 gpg
-    GCSFUSE_REPO=gcsfuse-$(lsb_release -c -s)
-    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt $GCSFUSE_REPO main" \
-    | sudo tee /etc/apt/sources.list.d/gcsfuse.list
-    if [ -f /usr/share/keyrings/cloud.google.gpg ]; then
-        curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor >> /usr/share/keyrings/cloud.google.gpg
-    else
-        curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
-    fi
-    apt-get update -y && apt-get install -y gcsfuse
-else
-    log "gcsfuse já está instalado, pulando instalação."
-fi
+pip3 install libesocial --break-system-packages
 
 log "Cria usuário Odoo e configura FUSE"
 if ! id odoo &>/dev/null; then
@@ -130,34 +101,14 @@ systemctl daemon-reload
 systemctl enable --now mnt-odooplugins.mount
 systemctl enable --now mnt-odooattachments.mount
 
-log "Instalando dependências do Odoo..."
-apt-get install -y git python3-pip python3-dev python3-venv \
-build-essential libxslt-dev libzip-dev libldap2-dev libsasl2-dev libssl-dev \
-libpq-dev libjpeg-dev nodejs npm fontconfig xfonts-75dpi xfonts-base wkhtmltopdf
-pip3 install sqlparse pandas validate_docbr simplejson --break-system-packages
-ln -sf /usr/bin/nodejs /usr/bin/node || true
-if ! npm install -g less less-plugin-clean-css 2> /tmp/npm_less_error.log; then
-    log "npm install failed: $(cat /tmp/npm_less_error.log)"
-    apt-get install -y node-less
-fi
-
-log "Instalando Odoo..."
-if [ -f /usr/share/keyrings/odoo-archive-keyring.gpg ]; then
-    rm -f /usr/share/keyrings/odoo-archive-keyring.gpg
-fi
-wget -qO- https://nightly.odoo.com/odoo.key | gpg --dearmor -o /usr/share/keyrings/odoo-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/odoo-archive-keyring.gpg] https://nightly.odoo.com/17.0/nightly/deb/ ./" > /etc/apt/sources.list.d/odoo.list
-apt-get update && apt-get install -y odoo
 ensure_dir "/opt/odoo17/addons" "odoo:odoo"
 ensure_dir "/var/lib/odoo" "odoo:odoo"
 ensure_dir "/var/log/odoo" "odoo:odoo"
-systemctl daemon-reload
-systemctl enable --now odoo.service
 cat > /etc/odoo/odoo.conf <<EOF
 [options]
 addons_path = /mnt/odoo-plugins,/opt/odoo17/addons
 data_dir    = /mnt/odoo-attachments
-admin_passwd = admin
+; admin_passwd = admin
 db_host     = $_DB_HOST
 db_port     = 5432
 db_user     = $_DB_USER
@@ -170,7 +121,9 @@ xmlrpc_interface = 0.0.0.0
 xmlrpc_port      = 8069
 limit_time_real = 1200
 limit_time_cpu = 1200
+db_filter = ^%odoo$
+list_db = False
 EOF
 chown odoo:odoo /etc/odoo/odoo.conf
 chmod 600 /etc/odoo/odoo.conf
-systemctl restart odoo.service
+systemctl enable --now odoo.service
